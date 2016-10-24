551711c130f7c37b4e39d60c6c9f224973f310d4
runnedrunner <auszhou@nuevaschool.org>
2016-10-24 08:55:03 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,23 +4,24 @@
 var name;
 var nameInput = $("#name-input").val();
 console.log(nameInput);
+console.log("stuff");
 var noteInput = $("#note-input")
 var noteList = $("#note-list")
 
-//nameInput.keydown(function(e) {
-//  if (e.which == 13) {
-//    // we have a new name! Now what?
-//  }	
-//})
-//
-//function getNotesForName(name) {  
-//  onNewListItem(name + "/notes", function(note) {
-//    console.log(note)
-//  })
-//}
-//
-//noteInput.keydown(function(e) {
-//  if (e.which == 13) {    
-//    addListItem(name + "/notes", noteInput.val())
-//  }
-//})
+nameInput.keydown(function(e) {
+  if (e.which == 13) {
+    // we have a new name! Now what?
+  }	
+})
+
+function getNotesForName(name) {  
+  onNewListItem(name + "/notes", function(note) {
+    console.log(note)
+  })
+}
+
+noteInput.keydown(function(e) {
+  if (e.which == 13) {    
+    addListItem(name + "/notes", noteInput.val())
+  }
+})
